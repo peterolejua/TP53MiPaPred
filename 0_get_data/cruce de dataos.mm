0503605079edbb374bdf4ffd5c0c0820730e788b
Peter Olejua <peterolejua@gmail.com>
2019-11-20 11:41:06 -0500

    Add files via upload

---
+++ 0_get_data/cruce de dataos.mm
@@ -0,0 +1,362 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="cruce" FOLDED="false" ID="ID_191153586" CREATED="1521989406520" MODIFIED="1538837171245" ICON_SIZE="36.0 pt" STYLE="oval">
+<font SIZE="22"/>
+<hook NAME="MapStyle" background="#ffd239" zoom="1.21">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64.0 pt" COLOR="#000000" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32.0 px" COLOR="#000000" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font NAME="Times New Roman" SIZE="18" BOLD="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font NAME="Times New Roman" SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font NAME="Times New Roman" SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24.0 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font NAME="Times New Roman" SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24.0 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font NAME="Times New Roman" SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24.0 px">
+<edge STYLE="bezier"/>
+<font NAME="Times New Roman"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16.0 px">
+<font NAME="Times New Roman" SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+<font NAME="Times New Roman"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+<font NAME="Times New Roman"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+<font NAME="Times New Roman"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+<font NAME="Times New Roman"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="110" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#ffffff"/>
+<node TEXT="mutations" POSITION="right" ID="ID_283897042" CREATED="1538837224074" MODIFIED="1538838352083">
+<edge COLOR="#007c00"/>
+<node TEXT="UMD_mutations_US.tsv" ID="ID_1874054763" CREATED="1538837264105" MODIFIED="1538837290869">
+<node TEXT="80406" OBJECT="java.lang.Long|80406" ID="ID_963709232" CREATED="1538837290874" MODIFIED="1538837299862">
+<node TEXT="not unique" ID="ID_1635103184" CREATED="1538837299866" MODIFIED="1538837302374"/>
+</node>
+<node TEXT="misMut" ID="ID_1860016174" CREATED="1538837343876" MODIFIED="1538837839946"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      filter missense
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="57885" OBJECT="java.lang.Long|57885" ID="ID_1531705976" CREATED="1538837361879" MODIFIED="1538837364453">
+<node TEXT="unique" ID="ID_227867325" CREATED="1538837420386" MODIFIED="1538837428098">
+<node TEXT="mis_variants" ID="ID_1580192258" CREATED="1538837428101" MODIFIED="1541261046784">
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_28260710" STARTINCLINATION="1261;0;" ENDINCLINATION="1261;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="1766" OBJECT="java.lang.Long|1766" ID="ID_1958228212" CREATED="1538837446018" MODIFIED="1538837448019"/>
+</node>
+<node TEXT="genome build" ID="ID_1087748521" CREATED="1541259624489" MODIFIED="1541260529661">
+<node TEXT="cDNA_Variant" ID="ID_1133642460" CREATED="1541259778326" MODIFIED="1541260529661" HGAP_QUANTITY="201.49999441206472 pt" VSHIFT_QUANTITY="-3.7499998882412946 pt">
+<node TEXT="reference sequence" ID="ID_1986844424" CREATED="1541259357168" MODIFIED="1541259379060">
+<node TEXT="NM_000546.5" ID="ID_928070067" CREATED="1541259379076" MODIFIED="1541260384747">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1581040860" STARTINCLINATION="163;341;" ENDINCLINATION="-82;55;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="ENST00000269305" ID="ID_320807588" CREATED="1541259992328" MODIFIED="1541259993026">
+<node TEXT="png_1396965788260291392.png" ID="ID_631685802" CREATED="1538857763000" MODIFIED="1538857763000">
+<hook URI="cruce%20de%20dataos_files/png_1396965788260291392.png" SIZE="0.4477612" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Uniprot_acc_Polyphen2==&quot;P04637&quot;" ID="ID_1386072265" CREATED="1541262062302" MODIFIED="1541262063416"/>
+</node>
+</node>
+</node>
+<node TEXT="HG18_Variant" ID="ID_924427620" CREATED="1541260094159" MODIFIED="1541260096021">
+<node TEXT="NCBI36/hg18" ID="ID_738891516" CREATED="1541259643762" MODIFIED="1541259645044">
+<node TEXT="NC_000017.9" ID="ID_236735641" CREATED="1541259701370" MODIFIED="1541259704415"/>
+</node>
+</node>
+<node TEXT="HG19_Variant" ID="ID_1716631935" CREATED="1541260170159" MODIFIED="1541260171308">
+<node TEXT="NCBI37/GRCh37/hg19" ID="ID_595943937" CREATED="1541259654316" MODIFIED="1546467063731">
+<node TEXT="NC_000017.10" ID="ID_1087808215" CREATED="1541259712173" MODIFIED="1541259712761"/>
+</node>
+</node>
+<node TEXT="HG38_Variant" ID="ID_634237587" CREATED="1541260110071" MODIFIED="1541260112774">
+<node TEXT="GRCh38.p2" ID="ID_194602506" CREATED="1541259663552" MODIFIED="1541259664793">
+<node TEXT="NC_000017.11" ID="ID_862194265" CREATED="1541259722178" MODIFIED="1541259722682"/>
+</node>
+</node>
+<node TEXT="NG_017013.2" ID="ID_1573152224" CREATED="1541259685469" MODIFIED="1541259686552"/>
+<node TEXT="Transcript t1 MN_000546.5" ID="ID_1133587550" CREATED="1541259822793" MODIFIED="1541259823590"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="transform to GRCH38" POSITION="right" ID="ID_1581040860" CREATED="1538837457761" MODIFIED="1538838352105" HGAP_QUANTITY="23.749999709427364 pt" VSHIFT_QUANTITY="118.4999964684249 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="mutalyzer" ID="ID_1193117945" CREATED="1538837484944" MODIFIED="1538837496865">
+<node TEXT="batch" ID="ID_1581815902" CREATED="1538837554799" MODIFIED="1543329627154"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1766
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="X2" ID="ID_1887762718" CREATED="1541260444266" MODIFIED="1541260449524">
+<node TEXT="NC_000017.11:g.7674181C&gt;T" ID="ID_944808327" CREATED="1541260458724" MODIFIED="1543330055337">
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_862194265" STARTINCLINATION="940;0;" ENDINCLINATION="940;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      position
+    </p>
+  </body>
+</html>
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_62929884" CREATED="1538837576797" MODIFIED="1543329638195" HGAP_QUANTITY="16.999999910593036 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0033">tp53gch38</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_28260710" STARTINCLINATION="-59;56;" ENDINCLINATION="162;4;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NO COVARIABLES
+    </p>
+    <p>
+      1766
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="input for dbnsfp program" ID="ID_1170960495" CREATED="1538837619149" MODIFIED="1538838390562">
+<node TEXT="umd_missense_gch38.out" ID="ID_1687577301" CREATED="1538837737018" MODIFIED="1538837738090">
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_479997064" CREATED="1538837738093" MODIFIED="1543330325135" TEXT_SHORTENED="true"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0033">scores</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_28260710" STARTINCLINATION="-207;174;" ENDINCLINATION="578;18;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  
+
+  <head>
+
+  </head>
+  <body>
+  </body>
+</html>
+</richcontent>
+<node TEXT="" ID="ID_1426203032" CREATED="1538838761745" MODIFIED="1538838761746">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Ensembl_transcriptid" ID="ID_622799094" CREATED="1538838613856" MODIFIED="1538838613856"/>
+<node TEXT="Ensembl_proteinid" ID="ID_1692101547" CREATED="1538838623125" MODIFIED="1538838623125"/>
+<node TEXT="" ID="ID_1961145507" CREATED="1538838761743" MODIFIED="1538838769666">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="22" OBJECT="java.lang.Long|22" ID="ID_1911413958" CREATED="1538838761746" MODIFIED="1538847505210" HGAP_QUANTITY="44.749999083578615 pt"/>
+</node>
+<node TEXT="sift_score" ID="ID_725569498" CREATED="1538839697060" MODIFIED="1538839714100">
+<node TEXT="number of scores" ID="ID_1387518342" CREATED="1538839714103" MODIFIED="1538855921552"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      6 possibilities
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="1" OBJECT="java.lang.Long|1" ID="ID_275321533" CREATED="1538847513707" MODIFIED="1538847515180">
+<node TEXT="sift.score" ID="ID_1266272980" CREATED="1538847522218" MODIFIED="1538847558027">
+<node TEXT="no data" ID="ID_35022698" CREATED="1538847558029" MODIFIED="1538847560796">
+<node TEXT="ENST00000455263" ID="ID_1233381212" CREATED="1538851446870" MODIFIED="1538851449688"/>
+<node TEXT="ENST00000420246" ID="ID_569580140" CREATED="1538851506997" MODIFIED="1538851532885"/>
+</node>
+</node>
+</node>
+<node TEXT="2" OBJECT="java.lang.Long|2" ID="ID_568034776" CREATED="1538847688936" MODIFIED="1538847694009">
+<node TEXT="sift.score=SIFT_score_04=SIFT_score_06" ID="ID_1936960929" CREATED="1538847730727" MODIFIED="1538847743608">
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_500332583" CREATED="1538851606866" MODIFIED="1538857859408"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0033">ENST00000269305</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="ENST00000445888" ID="ID_385763807" CREATED="1538851656176" MODIFIED="1538851658002"/>
+</node>
+</node>
+<node TEXT="6" OBJECT="java.lang.Long|6" ID="ID_1678616324" CREATED="1538847790037" MODIFIED="1538847798231">
+<node TEXT="sift.score=SIFT_score_09=SIFT_score_16" ID="ID_1771811450" CREATED="1538847730727" MODIFIED="1538850043628">
+<node TEXT="ENST00000269305" ID="ID_1488902470" CREATED="1538852882129" MODIFIED="1538852893937"/>
+<node TEXT="ENST00000445888" ID="ID_1943340469" CREATED="1538852928959" MODIFIED="1538852930560"/>
+</node>
+</node>
+<node TEXT="7" OBJECT="java.lang.Long|7" ID="ID_116613972" CREATED="1538850655259" MODIFIED="1538850658828">
+<node TEXT="sift.score=SIFT_score_10=SIFT_score_17" ID="ID_1105911600" CREATED="1538847730727" MODIFIED="1538853285496">
+<node TEXT="ENST00000269305" ID="ID_372674134" CREATED="1538853318933" MODIFIED="1538853320200"/>
+<node TEXT="ENST00000445888" ID="ID_276139933" CREATED="1538853302950" MODIFIED="1538853303991"/>
+</node>
+</node>
+<node TEXT="8" OBJECT="java.lang.Long|8" ID="ID_891461102" CREATED="1538854719829" MODIFIED="1538854722134">
+<node TEXT="png_3058888230011550546.png" ID="ID_1645809792" CREATED="1538855792417" MODIFIED="1538855792417">
+<hook URI="cruce%20de%20dataos_files/png_3058888230011550546.png" SIZE="0.4106776" NAME="ExternalObject"/>
+</node>
+<node TEXT="SIFT_score_03, SIFT_score_04, SIFT_score_10" ID="ID_1561845810" CREATED="1538856259877" MODIFIED="1538856261303">
+<node TEXT="ENST00000269305" ID="ID_385099672" CREATED="1538853318933" MODIFIED="1538853320200"/>
+</node>
+</node>
+<node TEXT="9" OBJECT="java.lang.Long|9" ID="ID_1706823773" CREATED="1538855928317" MODIFIED="1538855929598">
+<node TEXT="png_3325178337055903677.png" ID="ID_1287421282" CREATED="1538856915305" MODIFIED="1538856915305">
+<hook URI="cruce%20de%20dataos_files/png_3325178337055903677.png" SIZE="0.88495576" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Conlusion" ID="ID_1780662008" CREATED="1538857749519" MODIFIED="1538857758768"/>
+</node>
+<node TEXT="errors" ID="ID_631092652" CREATED="1543330220162" MODIFIED="1543332983239"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1764
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="two mutations not found" ID="ID_244607946" CREATED="1543330224680" MODIFIED="1543330229780">
+<node TEXT="c.993+295C&gt;T" ID="ID_144108386" CREATED="1543330231265" MODIFIED="1543330243540">
+<node TEXT="not aminoacid substitution" ID="ID_1543581542" CREATED="1543330380766" MODIFIED="1543330389808"/>
+</node>
+<node TEXT="c.637C&gt;T" ID="ID_1004567020" CREATED="1543332958052" MODIFIED="1543332962230">
+<node TEXT="nonsense mutation (wrong classification in UMD)" ID="ID_1505310827" CREATED="1543332971779" MODIFIED="1543332974630"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="merged" POSITION="right" ID="ID_1953063470" CREATED="1538838084049" MODIFIED="1538838352130" HGAP_QUANTITY="-1.7499995306134366 pt" VSHIFT_QUANTITY="96.7499971166254 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="data" ID="ID_28260710" CREATED="1538838088913" MODIFIED="1538838090322"/>
+</node>
+</node>
+</map>
